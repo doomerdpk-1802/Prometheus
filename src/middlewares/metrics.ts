0294b4ea30bed6ae0f42e7c8d561f80d7a33d0fc
Deepak Kumar <doomerdpk@gmail.com>
2026-03-29 13:10:05 +0530

    Exposed Histogram metric

--- src/middlewares/metrics.ts
+++ src/middlewares/metrics.ts
@@ -1,20 +1,34 @@
 import { Request, Response, NextFunction } from "express";
 import { requestCounter } from "./metrics/request.js";
 import { activeRequestsGauge } from "./metrics/activeRequests.js";
+import { httpReqDurationMs } from "./metrics/req_duration.js";
 
 export function metricsMiddleware(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
+  const startTime = Date.now();
   activeRequestsGauge.inc();
 
   res.on("finish", () => {
+    const endTime = Date.now();
+    const duration = endTime - startTime;
+
     requestCounter.inc({
       method: req.method,
       route: req.route?.path,
       status_code: res.statusCode,
     });
+
+    httpReqDurationMs.observe(
+      {
+        method: req.method,
+        route: req.route.path,
+        status_code: res.statusCode,
+      },
+      duration,
+    );
 
     activeRequestsGauge.dec();
   });
